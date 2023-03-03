e501b06ec49cf8425e17cc99322e0b2d6c42cb43
SiphoH1zn <aa.chonco@gmail.com>
2023-03-03 15:09:52 -0800

    Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * Main - last digit of randomly generated number
+ * check if its greater than 5, less than 6 or 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int m;
+	m = n % 10;
+
+	if (m > 5)
+        {
+                printf("Last digit of %d is %d greater than 5\n", n, m);
+        }
+        else if (m == 0)
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+        }
+        else
+        {
+                printf("Last digit of %d is %d and is 0\n", n, m);
+        }
+
+
+	return (0);
+}
